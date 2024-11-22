b79b867fd8d68a0ff5bcc370750e356066dec10b
Daniil Cheban <144154258+DaCheb1@users.noreply.github.com>
2024-11-22 17:55:41 +0300

    Add files via upload

---
+++ Class1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace Privet123
+{
+    public class Person
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string Country { get; set; }
+
+        public Person(string name, int age, string country)
+        {
+            Name = name;
+            Age = age;
+            Country = country;
+        }
+
+        public int GetBirthYear()
+        {
+            int currentYear = DateTime.Now.Year;
+            return currentYear - Age;
+        }
+
+        public string ToJson()
+        {
+            return JsonConvert.SerializeObject(this);
+        }
+    }
+}
---
+++ UnitTest1.cs
@@ -0,0 +1,32 @@
+﻿using NUnit.Framework;
+using Privet123;
+using System;
+using System.IO.Packaging;
+
+
+
+public class PersonTests
+{
+    private Person person;
+
+    [SetUp]
+    public void SetUp()
+    {
+        person = new Person("Alice", 30, "USA");
+    }
+
+    [Test]
+    public void TestGetBirthYear()
+    {
+        int currentYear = DateTime.Now.Year;
+        int expectedBirthYear = currentYear - person.Age;
+        Assert.AreEqual(expectedBirthYear, person.GetBirthYear());
+    }
+
+    [Test]
+    public void TestToJson()
+    {
+        string expectedJson = "{\"Name\":\"Alice\",\"Age\":30,\"Country\":\"USA\"}";
+        Assert.AreEqual(expectedJson, person.ToJson());
+    }
+}
